f5ef58442d6f2b6e2360ff5093f92d5cba0d85e8
coolightman <drugak48@gmail.com>
2022-12-02 22:41:11 +0300

    update

--- app/src/main/java/by/coolightman/qrscanner/MainActivity.kt
+++ app/src/main/java/by/coolightman/qrscanner/MainActivity.kt
@@ -3,6 +3,7 @@ package by.coolightman.qrscanner
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Size
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -15,41 +16,68 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import by.coolightman.qrscanner.ui.theme.QRScannerTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         setContent {
-            QRScannerTheme {
+            QRScannerTheme(darkTheme = true) {
+                val systemUiController = rememberSystemUiController()
+                val barsColor = MaterialTheme.colors.background
+
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = barsColor,
+                        darkIcons = false
+                    )
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val context = LocalContext.current
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    val clipboardManager = LocalClipboardManager.current
 
                     var code by remember {
                         mutableStateOf("")
                     }
-                    val context = LocalContext.current
-                    val lifecycleOwner = LocalLifecycleOwner.current
+                    LaunchedEffect(code) {
+                        clipboardManager.setText(AnnotatedString(code))
+                    }
+
                     val cameraProviderFuture = remember {
                         ProcessCameraProvider.getInstance(context)
                     }
+
                     var hasCameraPermission by remember {
                         mutableStateOf(
                             ContextCompat.checkSelfPermission(
@@ -82,10 +110,7 @@ class MainActivity : ComponentActivity() {
                                     preview.setSurfaceProvider(previewView.surfaceProvider)
                                     val imageAnalysis = ImageAnalysis.Builder()
                                         .setTargetResolution(
-                                            Size(
-                                                previewView.width,
-                                                previewView.height
-                                            )
+                                            Size(1280, 720)
                                         )
                                         .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
                                         .build()
@@ -111,7 +136,16 @@ class MainActivity : ComponentActivity() {
                                 },
                                 modifier = Modifier.weight(1f)
                             )
-                            Text(text = code)
+                            SelectionContainer {
+                                Text(
+                                    text = code,
+                                    fontSize = 18.sp,
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(12.dp)
+                                )
+                            }
                         }
                     }
                 }
