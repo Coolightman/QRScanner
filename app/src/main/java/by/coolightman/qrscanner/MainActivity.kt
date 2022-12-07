d7d04bb1636e33e70860ff5ee5a4b19040f32710
coolightman <drugak48@gmail.com>
2022-12-07 11:38:55 +0300

    update

--- app/src/main/java/by/coolightman/qrscanner/MainActivity.kt
+++ app/src/main/java/by/coolightman/qrscanner/MainActivity.kt
@@ -5,20 +5,26 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
@@ -29,10 +35,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import by.coolightman.qrscanner.component.AppCameraView
 import by.coolightman.qrscanner.ui.theme.QRScannerTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -78,8 +84,55 @@ class MainActivity : ComponentActivity() {
                     var code by remember {
                         mutableStateOf("")
                     }
-                    LaunchedEffect(code) {
-                        clipboardManager.setText(AnnotatedString(code))
+                    var isCopyToClipboard by remember {
+                        mutableStateOf(false)
+                    }
+                    LaunchedEffect(isCopyToClipboard) {
+                        if (isCopyToClipboard) {
+                            clipboardManager.setText(AnnotatedString(code))
+                            Toast.makeText(context, "Сopied to clipboard", Toast.LENGTH_SHORT)
+                                .show()
+                            isCopyToClipboard = false
+                        }
+                    }
+
+                    var isShowFindDialog by remember {
+                        mutableStateOf(false)
+                    }
+
+                    if (isShowFindDialog) {
+                        AlertDialog(title = {
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(text = "QR-code found")
+                                Icon(
+                                    painter = painterResource(R.drawable.ic_baseline_content_copy_24),
+                                    contentDescription = "copy",
+                                    modifier = Modifier
+                                        .size(18.dp)
+                                        .clickable { isCopyToClipboard = true }
+                                )
+                            }
+                        }, text = {
+                            Text(text = code)
+                        }, onDismissRequest = {
+                            isShowFindDialog = false
+                        }, confirmButton = {
+                            TextButton(onClick = {
+                                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(code))
+                                startActivity(browserIntent)
+                            }) {
+                                Text(text = "Open in browser")
+                            }
+                        }, dismissButton = {
+                            TextButton(onClick = { isShowFindDialog = false }) {
+                                Text(text = "Cancel")
+                            }
+                        }, shape = MaterialTheme.shapes.small
+                        )
                     }
 
                     Column(
@@ -88,25 +141,15 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize()
                     ) {
                         if (cameraPermissionState.status.isGranted) {
-                            AppCameraView(
-                                cameraProviderFuture = cameraProviderFuture,
-                                onGetResult = { code = it }
-                            )
-                            SelectionContainer {
-                                Text(
-                                    text = code,
-                                    fontSize = 18.sp,
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(12.dp)
-                                )
-                            }
+                            AppCameraView(cameraProviderFuture = cameraProviderFuture,
+                                onGetResult = {
+                                    code = it
+                                    isShowFindDialog = true
+                                })
                         } else {
                             if (cameraPermissionState.status.shouldShowRationale) {
                                 Text(
-                                    text = "QR-code can't work without this permission.\n" +
-                                            "Please grant the permission",
+                                    text = "QR-code can't work without this permission.\n" + "Please grant the permission",
                                     textAlign = TextAlign.Center,
                                     modifier = Modifier
                                         .fillMaxWidth()
@@ -122,8 +165,7 @@ class MainActivity : ComponentActivity() {
 
                             } else {
                                 Text(
-                                    text = "Sorry, but QR-code can't work without this permission.\n" +
-                                            "You can change the permission in settings",
+                                    text = "Sorry, but QR-code can't work without this permission.\n" + "You can change the permission in settings",
                                     textAlign = TextAlign.Center,
                                     modifier = Modifier
                                         .fillMaxWidth()
